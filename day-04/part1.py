eddd039c392a269498e8bda8f8f9ee6728a00449
martin98m <50966426+martin98m@users.noreply.github.com>
2023-12-04 16:48:53 +0000

    day 04 done - fin

--- day-04/part1.py
+++ day-04/part1.py
@@ -1,24 +1,20 @@
 input ='inputs/test.txt'
 input ='inputs/input.txt'
 
-
 with open(input, 'r') as f:
     file = f.read().splitlines()
-    
-    cardCount = [1 for e in range(len(file))]
+
+    cardCount = [1] * len(file)
     total = 0
     for i, line in enumerate(file):
         line = line.split(':')[1].split('|')
-        win, my = line[0].split(), line[1].split() 
+        win, my = line[0].split(), line[1].split()
+
+        count = len([e for e in my if e in win])
+        if count > 0:
+            total = total + 2**(count-1)
+            for x in range(i + 1, i + count + 1):
+                cardCount[x] = cardCount[x] + cardCount[i]
     
-        real = [e for e in my if e in win]
-        #print(win, my)
-        cnt = len(real)
-        if cnt > 0:
-            total = total + (1 if cnt == 1 else 2**(cnt-1))
-            s = i
-            for x in range(i+1, i+cnt+1):
-                cardCount[x] = cardCount[x] + cardCount[s] *1
-        
     print(total)
     print(sum(cardCount))
